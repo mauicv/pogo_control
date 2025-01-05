9a76afc107a38ae1c3b80d80a82fde13e08a2041
Alex Athorne <a.thornysort@gmail.com>
2025-01-05 12:35:17 +0000

    Update pin limits to full range

--- src/cli/__init__.py
+++ src/cli/__init__.py
@@ -184,10 +184,8 @@ def move_robot(front_left_bottom, front_left_top, front_right_bottom, front_righ
     ])
 
     # pogo move-robot --back-right-top=0.2 --front-right-top=-0.3
-
     time.sleep(3)
-    # servo.update_angle()
-
+    pogo.deinit()
 
 if __name__ == "__main__":
     cli()
