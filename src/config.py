202a391246eab7207c98d3fe55187e42ee233cfa
Alex Athorne <a.thornysort@gmail.com>
2025-05-06 17:12:38 +0100

    update starting posistion

--- src/config.py
+++ src/config.py
@@ -7,4 +7,4 @@ PRECOMPUTED_STDS = torch.tensor([[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]])
 
 INITIAL_ACTION = (0, 0, 0, 0, 0, 0, 0, 0)
 
-INITIAL_POSITION= (-0.4, -0.4, 0.4, 0.4, -0.4, -0.4, 0.4, 0.4)
+INITIAL_POSITION= (-0.4, 0.4, -0.4, 0.4, -0.4, 0.4, -0.4, 0.4)
